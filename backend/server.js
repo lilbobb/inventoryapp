4d26c92357dcc010d601c3d7fff56f1c788114bb
lilbobb <ukurowo@gmail.com>
2023-12-07 14:30:22 +0100

    Your commit message

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+// Connect to MongoDB and start server
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server Running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
+
